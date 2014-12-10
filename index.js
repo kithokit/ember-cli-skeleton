6a5a55ba4791e336f7b8a050ebdfffe3b734f639
Kit Ho <kithokit@mangowish.com>
2014-12-11 00:48:54 +0800

    add skeleton file

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 
 module.exports = {
   name: 'ember-cli-skeleton'
+   included: function(app) {
+     this._super.included(app);
+     var skeletonCSSpath   = 'vendor/skeleton/css/';
+     app.import(skeletonCSSpath, 'skeleton.css');
+     app.import(skeletonCSSpath, 'normailize.css');
+   }
 };
