4bfb6fc73fb78aa57b28059e929b2f0636b357c0
Andres Serrano <andres.srg@gmail.com>
2014-12-30 20:46:44 -0500

    Import normalize.css before skeleton.css

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
     var skeletonCSSpath   = 'vendor/skeleton/css/';
-    app.import(path.join(skeletonCSSpath, 'skeleton.css'));
     app.import(path.join(skeletonCSSpath, 'normalize.css'));
+    app.import(path.join(skeletonCSSpath, 'skeleton.css'));
   }
 };
